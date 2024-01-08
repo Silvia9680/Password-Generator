24f139f2a17b9863c6150db8201fac53d3012bcc
Silvia9680 <silviacarolina9680@gmail.com>
2024-01-08 08:57:05 -0300

    Add JavaScript

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const empty = "";
+const uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lCase = "abcdefghijklmnnpqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@·$%&/()=-_";
+
+const pLength = document.getElementById("p-length");
+const upperCase = document.getElementById("p-uppercase");
+const lowerCase = document.getElementById("p-lowercase");
+const pNumber = document.getElementById("p-number");
+const pSymbol = document.getElementById("p-symbol");
+const submit = document.getElementById("submit");
+const password = document.getElementById("password");
+
+submit.addEventListener("click", () => {
+    let initialPassword = empty;
+    (upperCase.checked) ? initialPassword += uCase : "";
+    (lowerCase.checked) ? initialPassword += lCase : "";
+    (pNumber.checked) ? initialPassword += number : "";
+    (pSymbol.checked) ? initialPassword += symbol : "";
+
+    password.value = generatePassword( pLength.value, 
+        initialPassword )
+});
+
+function generatePassword(l, initialPassword) {
+    let pass = "";
+    for (let i = 0; i < l; i++) {
+        pass += initialPassword.charAt( Math.floor( Math.
+            random() * initialPassword.length ))
+    }
+    return pass;
+}
+
+
+
